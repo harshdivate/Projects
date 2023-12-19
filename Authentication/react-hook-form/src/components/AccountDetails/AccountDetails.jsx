ce2bc0355bb3ae8557484d94214e53eba8f3bae5
Harsh <harshdivate45@gmail.com>
2023-12-19 21:23:11 +0530

    account details

--- Authentication/react-hook-form/src/components/AccountDetails/AccountDetails.jsx
+++ Authentication/react-hook-form/src/components/AccountDetails/AccountDetails.jsx
@@ -1,53 +1,58 @@
-import React, { useEffect ,useContext} from 'react'
+import React, { useEffect ,useContext,useState } from 'react'
 import userContext from '../../context/userContext'
+import './AccountDetails.css';
 
 
 function AccountDetails() {
     const {getUserLoginDetails} = useContext(userContext);
+    const [data,setData] = useState([])
     useEffect(()=>{
         const fetchData = async () => {
             const {sessionId,userId} = getUserLoginDetails();
             console.log(sessionId,userId)
-            // const options = {
-            //     method : "POST",
-            //     body : JSON.stringify({
-            //         "userId":"657223d052b5f0d7ee32"
-            //     }),
-            //     headers: {
-            //         "Content-type": "application/json; charset=UTF-8"
-            //     }
-            // }
-            // const data = await fetch('http://localhost:4500/getUserDetails',options);
-            // const d = await data.json();
-            // console.log(d);
-            fetch("http://localhost:4500/getUserDetails", {
-     
-    // Adding method type
-    method: "POST",
-     
-    // Adding body or contents to send
-    body: JSON.stringify({
-        title: "foo",
-        body: "bar",
-        userId: 1
-    }),
-     
-    // Adding headers to the request
-    headers: {
-        "Content-type": "application/json; charset=UTF-8"
-    }
-})
- 
-// Converting to JSON
-.then(response => response.json())
- 
-// Displaying results to console
-.then(json => console.log(json));
+            const options = {
+                method : "POST",
+                body : JSON.stringify({
+                    "userId":userId,
+                }),
+                headers: {
+                    "Content-type": "application/json; charset=UTF-8"
+                }
+            }
+            const result = await fetch('http://localhost:4500/getUserDetails',options);
+            const d = await result.json();
+            setData(d)
+            
         }
         fetchData();
     },[])
   return (
-    <div className='h-screen text-xl text-white bg-black'>AccountDetails</div>
+    <div className=' mx-auto bg-black text-white' >
+        <div className=' text-4xl  text-center'>AccountDetails</div>
+        <div className='h-screen flex justify-center items-center'>
+            <div className='w-1/4 h-[400px] border flex flex-col justify-around items-start border-white'>
+                <div className="m-4  flex text-2xl font-bold w-full justify-center items-center  ">
+                   <div>Welcome {data.name}</div>
+                            
+                </div>
+                   
+                <div className='m-4'>
+                    <div className='text-lg'>Name</div>
+                    <div className='text-xl text-black p-2 rounded-lg bg-white'>{data.name}</div>
+                    
+                </div>
+                <div className='m-4'>
+                    <div className='text-lg'>Email</div>
+                    <div className='text-xl text-black p-2 rounded-lg bg-white'>{data.email}</div>
+                </div>
+                <div className='m-4'>
+                    <div className='text-lg'>Phone</div>
+                    <div className='text-xl text-black p-2 rounded-lg bg-white'>{data.phone}</div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
   )
 }
 
--- Authentication/server/server.js
+++ Authentication/server/server.js
@@ -2,17 +2,21 @@ import configureAppwrite from "./appwrite/configureAppwrite.js";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import express from "express";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+  })
+);
 const account = configureAppwrite();
 
 app.post("/getUserDetails", async (req, res) => {
-  console.log(req.body);
   const data = await account.get(req.body.userId);
-
   res.json(data);
 });
 
