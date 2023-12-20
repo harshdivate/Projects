fa435ced19695010beb3eecdbf1c43a6912cb578
Harsh <harshdivate45@gmail.com>
2023-12-20 11:08:03 +0530

    details fetched from api

--- Authentication/react-hook-form/src/components/MovieDetails/MovieDetails.jsx
+++ Authentication/react-hook-form/src/components/MovieDetails/MovieDetails.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import getMovieDetails from '../../utils/movie-db/getMovieDetails';
+
 
 function MovieDetails() {
-    const {id} = useParams();
+  const [movieDetails,setMovieDetails] = useState({})
+  const [imageURL ,setImgUrl] = useState('https://image.tmdb.org/t/p/w1280')
+  const {id} = useParams();
+
+  useEffect(()=>{
+    const fetchData = async () => {
+        const {getDetailsWithId} = getMovieDetails();
+        const data =await  getDetailsWithId(id);
+        setMovieDetails(data);
+        setImgUrl(`https://image.tmbd.org/t/p/w1280${data.backdrop_path}`)
+    }
+    fetchData();
+  },[])
+  
   return (
-    <div className=' container'>
-        <div className=''>sf</div>
+    <div className='back-drop' style={{backgroundImage:`url(${imageURL})`}}>
+        <div className='bg-[#010100] text-white h-screen cont' >
+            <div className='bg-red'>{movieDetails.budget} sfds</div>
+        </div>
     </div>
   )
 }
 
 export default MovieDetails
+
+// style={{backgroundImage:`url(${imageURL}/${id})`}}
